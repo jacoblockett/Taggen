b54f906550dd04eb4cdc58a6a07c121467cf295c
Jacob Lockett <jacobwyattlockett@gmail.com>
2018-11-16 19:08:10 +0000

    added .sibling() and .child(); updated fx() naming schema

--- node_writer.js
+++ node_writer.js
@@ -1,62 +1,134 @@
 class Writer {
   constructor() {
     this.current_parent_node = ''
-    this.current_node = []
+    this.path = ''
+    this.unique = 0
     this.runner = {}
     this.product = ''
   }
 
-  static node(name) {
+  static node(id, name) {
     return {
       name,
       attributes: {},
-      children: {}
+      children: {},
+      inner: '',
+      unique: id
     }
   }
 
-  static return_nested(iterable, runner) {
-    return iterable.reduce((obj, property) => obj[property], runner)
+  static return_nested(path, runner) {
+    return path.split('.').reduce((obj, property) => obj[property], runner)
   }
 
-  static set_nested(obj, keys, value) {
+  static set_nested(obj, keys, id, value, flag) {
+    keys = keys.split('.')
     if (keys.length === 1) {
-      obj[keys[0]] = value
+      if (flag === 'c') {
+        obj[keys[0]] = Writer.node(id, value)
+      } else if (flag === 'a') {
+        obj[keys[0]] = value
+      } else {
+        throw new Error(`Invalid flag "${flag}"`)
+      }
     } else {
       const key = keys.shift()
 
-      obj[key] = Writer.set_nested(typeof obj[key] === 'undefined' ? {} : obj[key], keys, value)
+      obj[key] = Writer.set_nested(typeof obj[key] === 'undefined' ? {} : obj[key], keys.join('.'), id, value, flag)
     }
 
     return obj
   }
 
-  create_node(name) {
+  static find_path(id, object, path = [], found = false) {
+  	const searcher = obj => {
+  		for (let prop in obj) {
+  			path.push(prop)
+
+  			if (obj[prop] === id) {
+  				found = true
+  				break
+  			} else if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+  				searcher(obj[prop])
+  				if (found) break
+  			}
+
+  			path.pop()
+  		}
+  	}
+
+  	searcher(object)
+
+  	return found ? path.slice(0, path.length - 1).join('.') : undefined
+  }
+
+  parent(name) {
     this.current_parent_node = `node${Object.keys(this.runner).length}`
-    this.current_node = [this.current_parent_node]
-    this.runner[this.current_parent_node] = Writer.node(name)
+    this.path = this.current_parent_node
+    this.runner[this.current_parent_node] = Writer.node(this.unique, name)
+    this.unique ++
 
     return this
   }
 
-  set_attr(key, value) {
-    Writer.set_nested(this.runner, [...this.current_node, 'attributes', key], value)
+  attr(key, value) {
+    Writer.set_nested(this.runner, `${this.path}.attributes.${key}`, null, value, 'a')
 
     return this
   }
 
-  create_child(name) {
-    this.current_node = [...this.current_node, 'children', `node${Object.keys(Writer.return_nested([...this.current_node, 'children'], this.runner)).length}`]
-    Writer.set_nested(this.runner, this.current_node, Writer.node(name))
+  inner(text) {
+
+  }
+
+  child(name) {
+    const node_copy = this.path
+    this.path = `${node_copy}.children.node${Object.keys(Writer.return_nested(`${node_copy}.children`, this.runner)).length}`
+
+    Writer.set_nested(this.runner, this.path, this.unique, name, 'c')
+    this.unique ++
 
     return this
   }
 
-  create_sibling(name) {
+  sibling(name, id) {
+    if (id) {
+      const path = Writer.find_path(id, this.runner)
 
+      if (path) {
+        const updated_path = path.split('.').slice(0, path.split('.').length - 1).join('.')
+
+        if (updated_path.split('.').length === 1) {
+          const msg = `".sibling()" cannot be called at the parent level - use ".parent()" instead\n`
+          throw new Error(msg)
+        } else {
+          this.path = `${updated_path}.node${Object.keys(Writer.return_nested(updated_path, this.runner)).length}`
+
+          Writer.set_nested(this.runner, this.path, this.unique, name, 'c')
+          this.unique ++
+        }
+      } else {
+        const msg = `Unique id "${id}" could not be found in the current schema. Try running ".preview()"\n`
+        throw new Error(msg)
+      }
+    } else {
+      const updated_path = this.path.split('.').slice(0, this.path.split('.').length - 1).join('.')
+      this.path = `${updated_path}.node${Object.keys(Writer.return_nested(updated_path, this.runner)).length}`
+
+      Writer.set_nested(this.runner, this.path, this.unique, name, 'c')
+      this.unique ++
+    }
+    // const node_copy = this.path
+    // this.path = `${node_copy}`
+    return this
   }
 
   preview() {
-    console.log(JSON.stringify(this.runner, null, 2))
+    console.log('\nPREVIEW:\n' + JSON.stringify({
+      parent: this.current_parent_node,
+      current_path: this.path,
+      current_schema: this.runner
+    }, null, 4) + '\n')
     return this
   }
 }
@@ -64,13 +136,10 @@ class Writer {
 const nw = new Writer()
 
 nw
-  .create_node('?xml')
-  .set_attr('asdf', 'fuckyou')
-  .set_attr('yeah', 'fuck you too')
-  .create_child('hello')
-  .create_child('child of child')
+  .parent('MOM')
+  .child('BOB')
+  .parent('DAD')
+  .child('TIM')
+  .child('CINDY')
+  .sibling('HI')
   .preview()
-  // .set_attr('Id', 'rId1')
-  // .set_attr('Id', 'rId2')
-  // .create_child('ChildHere')
-  // .commit()
